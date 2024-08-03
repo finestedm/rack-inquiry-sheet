735ad18d53a0ccd105a210871a93e79b44e0719a
Pawel <finestedm@gmail.com>
2024-08-03 23:43:10 +0200

    mezzanine config component sizing fixed

--- src/components/form/systemStep/subcomponents/MezzanineConfigs/MezzanineLevelsConfigs.tsx
+++ src/components/form/systemStep/subcomponents/MezzanineConfigs/MezzanineLevelsConfigs.tsx
@@ -5,6 +5,8 @@ import LevelConfigDrawing from "../RackConfigs/LevelConfigDrawing";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../../features/redux/store";
 import { useTranslation } from "react-i18next";
+import InputGroup from "../../../InputGroup";
+import { t } from "i18next";
 
 export default function MezzanineLevelsConfigs({ selectedSystem }: { selectedSystem: keyof ISystems }) {
     const formData = useSelector((state: RootState) => state.formData)
@@ -16,15 +18,20 @@ export default function MezzanineLevelsConfigs({ selectedSystem }: { selectedSys
 
     if (selectedSystem === 'mezzanine') {
         return (
-            <Stack spacing={2}>
-                <Box>
-                    <LevelsConfigTable selectedSystem={selectedSystem} configId={levelConfigs[0].id} />
-                </Box>
-                <Box sx={{ px: 2 }}>
-                    <Typography gutterBottom variant="h6" color="textPrimary">Podgląd</Typography>
-                    {selectedLevelConfig && <LevelConfigDrawing selectedSystem={selectedSystem} levels={selectedLevelConfig.levels} />}
-                </Box>
-            </Stack>
+            <InputGroup
+                title={t(`system.subheader.mezzanineConfigs`)}
+                content={
+                    <Stack spacing={2}>
+                        <Box>
+                            <LevelsConfigTable selectedSystem={selectedSystem} configId={levelConfigs[0].id} />
+                        </Box>
+                        <Box sx={{ px: 2 }}>
+                            <Typography gutterBottom variant="h6" color="textPrimary">Podgląd</Typography>
+                            {selectedLevelConfig && <LevelConfigDrawing selectedSystem={selectedSystem} levels={selectedLevelConfig.levels} />}
+                        </Box>
+                    </Stack>
+                }
+            />
         )
     } else {
         return null
