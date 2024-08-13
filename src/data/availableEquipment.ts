b2a39bc549c6e0d2070d053911303f098e6c1b0f
Paweł <finestedm@gmail.com>
2024-08-13 12:38:35 +0200

    new accessories

--- src/data/availableEquipment.ts
+++ src/data/availableEquipment.ts
@@ -1,7 +1,7 @@
 import { IEquipment } from "../features/interfaces";
 
  const availableEquipment = [
-    'dock', 'wall', 'gate', 'storage area', 'racking area', 'charging stations', 'pickup station', 'unload station'
+    'dock', 'wall', 'gate', 'storage area', 'racking area', 'charging stations', 'pickup station', 'unload station', 'mezzanine', 'starcase'
 ]
 
 export default availableEquipment
